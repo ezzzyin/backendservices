07acc6c80200f4c85dc9e07f7de50847c80ad07d
premprakash999 <premprakash999@gmail.com>
2019-01-27 11:48:38 -0500

    //First

---
+++ src/main/java/com/ezzzy/restservice/EzzzyApplication.java
@@ -0,0 +1,17 @@
+package com.ezzzy.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+public class EzzzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzzzyApplication.class, args);
+	}
+
+	
+}
+
